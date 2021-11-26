78b2ae6604be30448c898c6ebdf8b5319e0996e3
diego <diegoteixeirapereira@hotmail.com>
2021-11-26 12:30:39 -0300

    criação do app android com exemplos de layout.

---
+++ 06-Layouts/app/src/main/java/br/java/a06_layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package br.java.a06_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exemplo_scroll);
+    }
+}
