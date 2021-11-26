d2717e711120c7734e34812097b47299dbe7997f
diego <diegoteixeirapereira@hotmail.com>
2021-11-26 10:10:13 -0300

    criação do app ActivityResult.

---
+++ ActivityResult/app/src/main/java/br/java/activityresult/TelaSelecaoActivity.java
@@ -0,0 +1,47 @@
+package br.java.activityresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+
+public class TelaSelecaoActivity extends ListActivity {
+
+    public static final String EXTRA_ESTADO = "estado";
+    public static final String EXTRA_RESULTADO = "result";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_tela_selecao);
+
+        String[] estados = getResources().getStringArray(R.array.estados);
+
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, estados));
+        String estado = getIntent().getStringExtra(EXTRA_ESTADO);
+
+        if (estado != null) {
+            int position = Arrays.asList(estados).indexOf(estado);
+            getListView().setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+            getListView().setItemChecked(position, true);
+        }
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String result = l.getItemAtPosition(position).toString();
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_RESULTADO, result);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
