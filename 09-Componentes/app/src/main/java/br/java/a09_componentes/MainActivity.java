404ced1da719532cbe6b163e305dd3e30c24e5a2
diego <diegoteixeirapereira@hotmail.com>
2021-11-26 16:08:28 -0300

    criação do app android com exemplo de componentes.

---
+++ 09-Componentes/app/src/main/java/br/java/a09_componentes/MainActivity.java
@@ -0,0 +1,104 @@
+package br.java.a09_componentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    CompoundButton checkBox;
+    ToggleButton toggleButton;
+    Switch switchButton;
+    SeekBar seekBar;
+    Spinner spinner;
+    RadioGroup radioGroup;
+    TextView txtValor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkBox = (CompoundButton) findViewById(R.id.chkHabilitar);
+        toggleButton = (ToggleButton) findViewById(R.id.tgbHabilitar);
+        switchButton = (Switch) findViewById(R.id.swtHabilitar);
+        seekBar = (SeekBar) findViewById(R.id.skbValor);
+        spinner = (Spinner) findViewById(R.id.spnNomes);
+        radioGroup = (RadioGroup) findViewById(R.id.rgOpcoes);
+        txtValor = (TextView) findViewById(R.id.txtValor);
+
+        configuraSpinner();
+        configuraSeekBar();
+        configuraSwitch();
+
+        // Atribuindo programaticamente os valores-padrão
+        checkBox.setChecked(true);
+        seekBar.setProgress(20);
+        spinner.setSelection(2);
+        radioGroup.check(R.id.rbOpcao2);
+
+    }
+
+    private void configuraSwitch() {
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                checkBox.setEnabled(compoundButton.isEnabled());
+                toggleButton.setEnabled(b);
+            }
+        });
+    }
+
+    private void configuraSeekBar() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtValor.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private  void configuraSpinner() {
+        String[] nomes = new String[] {"Kurt", "Amy", "Max", "Bob", "Peter"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nomes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void verValores(View v) {
+        int idRadioSelecionado = radioGroup.getCheckedRadioButtonId();
+        RadioButton radio = (RadioButton) findViewById(idRadioSelecionado);
+        String habilitado = checkBox.isChecked() ? "Habilitado" : "Desabilitado";
+        String valor = "valor: " + seekBar.getProgress();
+        String nome = "nome: " + spinner.getSelectedItem().toString();
+        String opcao = "opcao: " + radio.getText();
+        StringBuilder mensagem = new StringBuilder();
+        mensagem.append(habilitado).append("\n")
+                .append(valor).append("\n")
+                .append(nome).append("\n")
+                .append(opcao);
+
+        Toast.makeText(this, mensagem.toString(), Toast.LENGTH_SHORT).show();
+    }
+}
