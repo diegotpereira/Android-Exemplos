79b038841a23e63d20fa384bbe2ff226b94b35c2
diego <diegoteixeirapereira@hotmail.com>
2021-11-26 17:33:12 -0300

    alteração no arquivo adapter.

--- 11-ExpandList/app/src/main/java/br/java/a11_expandlist/adapter/MeuExpandableAdapter.java
+++ 11-ExpandList/app/src/main/java/br/java/a11_expandlist/adapter/MeuExpandableAdapter.java
@@ -15,82 +15,75 @@ public class MeuExpandableAdapter extends BaseExpandableListAdapter {
 
     private Map<String, List<String>> dados;
     private List<String> keys;
-
     public MeuExpandableAdapter(Map<String, List<String>> dados) {
         this.dados = dados;
-        this.keys = new ArrayList<String>(dados.keySet());
+        this.keys = new ArrayList<String>(
+                dados.keySet());
     }
-
-    @Override
-    public int getGroupCount() {
-        return dados.size();
-    }
-
-    @Override
-<<<<<<< HEAD
-    public int getChildrenCount(int groupPosition) {
-        return dados.get(keys.get(groupPosition)).size();
-=======
-    public int getChildrenCount(int i) {
-        return 0;
->>>>>>> ab7778d07258004f4ae4f88fee54ef4bd811d823
-    }
-
     @Override
     public Object getGroup(int groupPosition) {
         return keys.get(groupPosition);
     }
-
     @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        return dados.get(keys.get(groupPosition)).get(childPosition);
+    public int getGroupCount() {
+        return dados.size();
     }
-
     @Override
     public long getGroupId(int groupPosition) {
         return groupPosition;
     }
-
+    @Override
+    public View getGroupView(int groupPosition,
+                             boolean isExpanded, View convertView,
+                             ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(
+                    parent.getContext()).inflate(
+                    android.R.layout.simple_expandable_list_item_1,
+                    null);
+        }
+        TextView txt = (TextView)
+                convertView.findViewById(android.R.id.text1);
+        txt.setTextColor(Color.WHITE);
+        txt.setBackgroundColor(Color.GRAY);
+        txt.setText(keys.get(groupPosition));
+        return convertView;
+    }
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return dados.get(
+                keys.get(groupPosition)).get(childPosition);
+    }
     @Override
     public long getChildId(int groupPosition, int childPosition) {
         return 0;
     }
-
+    @Override
+    public View getChildView(int groupPosition,
+                             int childPosition, boolean isLastChild,
+                             View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(
+                    parent.getContext()).inflate(
+                    android.R.layout.simple_list_item_1, null);
+        }
+        TextView txt = (TextView)
+                convertView.findViewById(android.R.id.text1);
+        txt.setText(dados.get(
+                keys.get(groupPosition)).get(childPosition));
+        return convertView;
+    }
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return dados.get(keys.get(groupPosition)).size();
+    }
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
-
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_expandable_list_item_1, null);
-        }
-
-        TextView txt = (TextView) convertView.findViewById(android.R.id.text1);
-        txt.setTextColor(Color.WHITE);
-        txt.setBackgroundColor(Color.GRAY);
-        txt.setText(keys.get(groupPosition));
-
-        return convertView;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, null);
-        }
-
-        TextView txt = (TextView) convertView.findViewById(android.R.id.text1);
-        txt.setText(dados.get(keys.get(groupPosition)).get(childPosition));
-
-        return convertView;
-    }
-
-    @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition) {
+    public boolean isChildSelectable(
+            int groupPosition, int childPosition) {
         return true;
     }
 }
