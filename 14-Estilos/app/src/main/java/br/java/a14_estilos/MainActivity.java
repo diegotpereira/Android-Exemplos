442e2a926174b50faa249807d71bb0db28b6e887
diego <diegoteixeirapereira@hotmail.com>
2021-11-28 17:32:51 -0300

    alteração no layout.

--- 14-Estilos/app/src/main/java/br/java/a14_estilos/MainActivity.java
+++ 14-Estilos/app/src/main/java/br/java/a14_estilos/MainActivity.java
@@ -3,8 +3,14 @@ package br.java.a14_estilos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
