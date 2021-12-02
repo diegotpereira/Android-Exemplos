40c45471f9d296c52fb9c633a4ffa279d46f1b0f
diego <diegoteixeirapereira@hotmail.com>
2021-12-02 16:13:47 -0300

    criação app android com exemplo de Broadcast receiver.

---
+++ 22-Broadcast/app/src/main/java/br/java/a22_broadcast/MainActivity.java
@@ -0,0 +1,67 @@
+package br.java.a22_broadcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String MINHA_ACAO =
+            "br.java.a22_broadcast.MINHA_ACAO";
+
+    InternoReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mReceiver = new InternoReceiver();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filterLocal = new IntentFilter(MINHA_ACAO);
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver, filterLocal);
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_USER_PRESENT);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
+    }
+
+    public void enviarBroadcast(View v) {
+        Intent it = new Intent(MINHA_ACAO);
+        sendBroadcast(it);
+    }
+
+    public void enviarLocalBroadcast(View v) {
+        Intent it = new Intent(MINHA_ACAO);
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(it);
+    }
+
+    class InternoReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView txtMensagem = (TextView) findViewById(R.id.txtMensagem);
+            txtMensagem.setText("Ação: \n" + intent.getAction());
+        }
+    }
+}
