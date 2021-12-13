d318bf89dfc63da787c1408f90172ab4e75d572d
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-13 14:08:13 -0300

    criação do app android com exemplo de envio de SMS.

---
+++ 35-SMS/app/src/main/java/br/java/a35_sms/receiver/SmsReceiver.java
@@ -0,0 +1,35 @@
+package br.java.a35_sms.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        SmsMessage sms = getMessagesFromIntent(it)[0];
+        String telefone = sms.getOriginatingAddress();
+        String mensagem = sms.getMessageBody();
+
+        Toast.makeText(context,
+                "Mensagem recebida de "
+                        + telefone + " - "
+                        + mensagem,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public static SmsMessage[] getMessagesFromIntent(Intent intent) {
+
+        Object[] pdusExtras = (Object[])intent.getSerializableExtra("pdus");
+        SmsMessage[] messages = new SmsMessage[pdusExtras.length];
+
+        for (int i = 0; i < pdusExtras.length; i++) {
+            messages[i] = SmsMessage.createFromPdu((byte[])pdusExtras[i]);
+        }
+
+        return messages;
+    }
+}
