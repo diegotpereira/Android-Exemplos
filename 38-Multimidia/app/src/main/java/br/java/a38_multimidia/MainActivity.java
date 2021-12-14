964d834ed07e5f214f489a6add224f4fe6eabfc3
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-14 13:31:34 -0300

    criação do app android com exemplo de Multimidia.

---
+++ 38-Multimidia/app/src/main/java/br/java/a38_multimidia/MainActivity.java
@@ -0,0 +1,14 @@
+package br.java.a38_multimidia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
