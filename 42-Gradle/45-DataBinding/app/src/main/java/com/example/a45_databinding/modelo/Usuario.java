d2ac3b9c38c874d0df3b1e307727d013080e1171
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-27 21:13:00 -0300

    criação do app com exemplo de Databinding.

---
+++ 42-Gradle/45-DataBinding/app/src/main/java/com/example/a45_databinding/modelo/Usuario.java
@@ -0,0 +1,15 @@
+package com.example.a45_databinding.modelo;
+
+import android.databinding.BaseObservable;
+import android.databinding.ObservableField;
+
+public class Usuario extends BaseObservable{
+
+    public ObservableField<String> nome = new ObservableField<>();
+    public ObservableField<String> sobrenome = new ObservableField<>();
+
+    public Usuario(String nome, String sobrenome) {
+        this.nome.set(nome);
+        this.sobrenome.set(sobrenome);
+    }
+}
